43c74f2e2c2a579b90cf6ed3aaf0b4d4f1528782
David <davidelbar2@gmail.com>
2021-02-17 13:44:29 +0100

    ok

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -4,19 +4,17 @@ function Body() {
       <div className="container-left">
         <p className="title-left">Une entreprise dans le low-tech</p>
         <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
-          elementum feugiat orci, non commodo sapien. Fusce viverra felis quis
-          orci rhoncus eleifend. Maecenas semper cursus tortor eget ullamcorper.
-          Vestibulum quis malesuada urna, eu rhoncus tellus. In ullamcorper
-          faucibus purus, eu cursus lorem ullamcorper non. Vivamus commodo nibh
-          eu elementum dictum.
+          Notre entreprise "Freco" est une entreprise qui a pour mission de
+          construire des garde-manger en bois afin de conserver tout type
+          d'aliments. Nous voulons donc remplacer le réfrigérateur électrique
+          par un garde-manger qui n'utilise très peu voir aucune énergie.
         </p>
       </div>
       <div className="container-center">
         <img src="gardemanger.jpg" alt="Garde-Manger" />
       </div>
       <div className="container-right">
-      <p className="title-right">Une entreprise dans le low-tech</p>
+        <p className="title-right">Une entreprise dans le low-tech</p>
         <p>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer
           elementum feugiat orci, non commodo sapien. Fusce viverra felis quis
